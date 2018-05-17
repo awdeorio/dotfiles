841698c651aee13379c43b8ce0ae02981353e8af
Andrew DeOrio <awdeorio@umich.edu>
2018-05-17 12:21:13 -0400

    Fix pdb vs. pdbpp configuration

--- .pdbrc.py
+++ .pdbrc.py
@@ -1,9 +1,13 @@
+"""
+pdbpp configuration
+
+# https://pypi.org/project/pdbpp/#description
+"""
 import pdb
-import rlcompleter
 
-# Tab completion
-pdb.Pdb.complete = rlcompleter.Completer(locals()).complete
-
-# Start sticky mode automatically when pdbpp is installed
 class Config(pdb.DefaultConfig):
+    # Start sticky mode automatically when pdbpp is installed
     sticky_by_default = True
+
+    # Colors: https://gist.github.com/chrisopedia/8754917#background
+    current_line_color = 42
