4dfa7720583a015baefbeead1f0bc5934123d7f1
Andrew DeOrio <awdeorio@umich.edu>
2018-11-07 11:18:51 -0500

    init.el tweak autoupdate

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -118,8 +118,9 @@
 (use-package auto-package-update
   :ensure t
   :config
-  (setq auto-package-update-delete-old-versions t
-        auto-package-update-interval 7)
+  (setq auto-package-update-delete-old-versions t)
+  ;;(setq auto-package-update-hide-results t)
+  (setq auto-package-update-interval 7)
   (auto-package-update-maybe)
   )
 
