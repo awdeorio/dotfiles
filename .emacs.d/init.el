d29ff3e82ce32d2df88292b084768f35e09710a1
Andrew DeOrio <awdeorio@umich.edu>
2019-10-05 17:04:26 -0400

    init.el remove marmalade packge repository because of broken SSL

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -89,7 +89,6 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (package-initialize)
 (when (not package-archive-contents)
